dda13bf9074ff6b06e01060fd74225030408a5c8
vanshika13varshney <162955765+vanshika13varshney@users.noreply.github.com>
2024-09-27 12:34:29 +0530

    Add files via upload

---
+++ LARGESTT.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a=7,b=6;
+  printf("a=%d and b=%d \n\n",a,b);
+  if(a>b)
+  printf("a is greater \n");
+  else
+  printf("b is geater.\n");
+  getch();
+  clrscr();
+}
