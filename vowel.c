82f4f3590a164c94ae0dc81bb52e7f40f64096be
vanshika13varshney <162955765+vanshika13varshney@users.noreply.github.com>
2024-10-04 12:27:18 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ char ch;
+ printf("enter any alphabet:\n");
+ scanf("%c",&ch);
+ switch (ch)
+ {
+ 	case 'a':
+ 		printf("vowel");
+ 		break;
+ 	case 'e':
+	 	printf("vowel");
+	 	break;
+	 case 'i':
+	     printf("vowel");
+		 break;
+	 case 'o':
+	     printf("vowel");
+		 break;
+	 case 'u':
+	     printf("vowel");
+		 break;	
+	 case 'A':
+ 		printf("vowel");
+ 		break;
+ 	case 'E':
+	 	printf("vowel");
+	 	break;
+	 case 'I':
+	     printf("vowel");
+		 break;
+	 case 'O':
+	     printf("vowel");
+		 break;
+	 case 'U':
+	     printf("vowel");
+		 break;	
+	 default:printf("consonent");
+ }
+}
